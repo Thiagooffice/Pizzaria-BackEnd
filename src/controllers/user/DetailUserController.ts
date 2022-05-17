16f8f5b30946839e7e3d11881319a5a661190ad9
thiago <thiagooffice2@gmail.com>
2022-05-16 22:33:43 -0300

    user-details

--- src/controllers/user/DetailUserController.ts
+++ src/controllers/user/DetailUserController.ts
@@ -4,9 +4,11 @@ import {DetailUserService} from '../../services/user/DetailUserService'
 class DetailUserController{
     async handle(req: Request, res: Response){
 
+        const user_id = req.user_id
+        
         const detaiUserService = new DetailUserService()
 
-        const user =  await detaiUserService.execute()
+        const user =  await detaiUserService.execute(user_id)
 
         return res.json(user)
 
