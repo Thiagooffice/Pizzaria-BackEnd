13a4065607370831ecde1f6852551c4a0b505931
thiago <thiagooffice2@gmail.com>
2022-05-16 23:24:09 -0300

    registering-categories

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,9 @@
 import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
-
 import { AuthUserController } from './controllers/user/AuthUserController';
-
 import {DetailUserController} from './controllers/user/DetailUserController' 
+import {CreateCategoryController} from './controllers/category/CreateCategoryController'
 
 import {isAuthenticated} from './meddlewares/isAuthenticated'
 
@@ -16,5 +15,8 @@ router.post('/users', new CreateUserController().handle)
 router.post('/session', new AuthUserController().handle)
 
 router.get('/me',isAuthenticated, new DetailUserController().handle)
+
+//rotas category
+router.post('/category', isAuthenticated, new CreateCategoryController().handle)
 
 export { router }; 
