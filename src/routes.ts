a0b44a181d7e42895ee1ea5152411a48202e0ba9
thiago <thiagooffice2@gmail.com>
2022-05-17 21:22:17 -0300

    Route-products

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,8 @@ import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController';
 import {DetailUserController} from './controllers/user/DetailUserController' 
 import {CreateCategoryController} from './controllers/category/CreateCategoryController'
+import {ListCategoryController} from './controllers/category/ListCategryController'
+import {CreateProductController} from './controllers/product/CreateProductController'
 
 import {isAuthenticated} from './meddlewares/isAuthenticated'
 
@@ -18,5 +20,10 @@ router.get('/me',isAuthenticated, new DetailUserController().handle)
 
 //rotas category
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
+
+//--rotas product
+router.post('/product', isAuthenticated, new CreateProductController().handle)
 
 export { router }; 
